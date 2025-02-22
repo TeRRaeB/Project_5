9dafb561e75fd5350f6efb7c2eab54ec9aac01a6
Terraeb <mihalyovvalik@gmail.com>
2025-02-22 18:53:46 +0000

    update and remove from bag

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.http import JsonResponse
+ 
 
 def view_bag(request):
     """a view to return the bag contents page"""
@@ -23,3 +23,30 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag 
     
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id): 
+
+    bag = request.session.get('bag', {})
+    quantity = int(request.POST.get('quantity'))  
+ 
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id): 
+
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id, None)
+
+        request.session['bag'] = bag
+
+        return JsonResponse({"success": True})
+
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
