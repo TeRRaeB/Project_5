946427ee8945767c40281ca96ae09ededf24af0a
Terraeb <mihalyovvalik@gmail.com>
2025-02-25 11:11:55 +0000

    99% finish checkout

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
         template = 'checkout/checkout.html'
         context = {
             'order_form': order_form,
+            'stripe_public_key': 'pk_test_51QwLg2RhkQw6p57bvpuKHUWIzuVVvvNUzX3t2o0QmvyBI7FrsnkHkNSYjnUkPoePwO7qRBHTdjXutQq4lWAHHnuR00tTB84q6Z',
+            'stripe_secret': 'sk_test_51QwLg2RhkQw6p57bzp83Xk9Djy5BrI8R3EAE29xi2kosm2E0ayfH1mqHaB4d5K6uG0FCzpEkJmQ7LIdNlT6wWy2W00GzAh8EsK',
             }
         
         return render(request,template,context)
