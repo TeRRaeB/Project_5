b83a283acec2d9adc09cad8aed8f432095b78f3d
Terraeb <mihalyovvalik@gmail.com>
2025-02-28 21:43:56 +0000

    heroku

--- golden_hoof/settings.py
+++ golden_hoof/settings.py
@@ -125,15 +125,6 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = "golden_hoof.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
