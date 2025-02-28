edeebbf6789e94f82aab8b19cb17104b42d19c66
Terraeb <mihalyovvalik@gmail.com>
2025-02-28 21:35:36 +0000

    HEroky

--- golden_hoof/settings.py
+++ golden_hoof/settings.py
@@ -33,17 +33,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["golden-hoof-e080ba2678ec.herokuapp.com",]
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 
 # Application definition
 
