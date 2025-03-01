70a525e56f359ddf718224c794b7657054d90860
Terraeb <mihalyovvalik@gmail.com>
2025-03-01 20:33:45 +0000

    heroku

--- golden_hoof/settings.py
+++ golden_hoof/settings.py
@@ -87,7 +87,14 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 # Email settings
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
 DEFAULT_FROM_EMAIL = "goldenhoof@example.com"
 
 # Password validation
@@ -147,4 +154,3 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-print("STATICFILES_DIRS:", STATICFILES_DIRS)
