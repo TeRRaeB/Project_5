e6e9a2dd6a4075ac3557cba8341549454d7a7314
Terraeb <mihalyovvalik@gmail.com>
2025-03-01 17:13:31 +0000

    heroku

--- golden_hoof/settings.py
+++ golden_hoof/settings.py
@@ -121,6 +121,7 @@ MEDIAFILES_LOCATION = 'media'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ROOT_URLCONF = 'golden_hoof.urls'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
