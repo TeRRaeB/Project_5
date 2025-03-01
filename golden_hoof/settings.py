efc555255bed582a1b6ebc4d0b98913afc1e7dfd
Terraeb <mihalyovvalik@gmail.com>
2025-03-01 18:26:52 +0000

    heroku

--- golden_hoof/settings.py
+++ golden_hoof/settings.py
@@ -112,6 +112,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_LOCATION = 'static'
 MEDIAFILES_LOCATION = 'media'
 
